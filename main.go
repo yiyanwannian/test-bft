b865d4eff7197744cd0a007b19804cd595c1234c
yongxinyao <yyxyong@163.com>
2021-08-20 20:55:19 +0800

    add recovery feature

--- main.go
+++ main.go
@@ -9,9 +9,11 @@ import (
 
 func main() {
 	var(
+		step int
 		dbPath string
 		accAddr string
 	)
+	flag.IntVar(&step, "step", 0, "0: modify amount; 1: recovery amount")
 	flag.StringVar(&dbPath, "db_path", "", "db of the path")
 	flag.StringVar(&accAddr, "user_addr", "", "acc of the addr")
 	flag.Parse()
@@ -28,41 +30,47 @@ func main() {
 		}
 		balanceKey := fmt.Sprintf("B/%s", accAddr)
 		underlayDBBalanceKey := append(append([]byte("SYSTEM_CONTRACT_DPOS_ERC20"), '#'), balanceKey...)
-		//fmt.Printf("key: %s, hash: %x\n", string(underlayDBBalanceKey), sha256.Sum256(underlayDBBalanceKey))
 
 		// 2. get the balance from db
 		val, err := db.Get(underlayDBBalanceKey, nil)
 		if err != nil {
 			continue
-			panic(fmt.Sprintf("get balance failed, error: %s", err))
 		}
 		balance, ok := big.NewInt(0).SetString(string(val), 10)
 		if !ok {
-			continue
 			panic(fmt.Sprintf("covert balance bytes[%s] to big.Int failed", balance))
 		}
 		fmt.Printf("before update balance: %s\n", balance.String())
 
 		// 3. update the balance in the db
-		if err = db.Put(underlayDBBalanceKey, []byte("0"), nil); err != nil {
-			continue
-			panic(fmt.Sprintf("update balance failed, error: %s", err))
+		if step == 0{
+			if err = db.Put(underlayDBBalanceKey, []byte("0"), nil); err != nil {
+				continue
+			}
+		}else if step ==1{
+			if err = db.Put(underlayDBBalanceKey, []byte("10"), nil); err != nil {
+				continue
+			}
 		}
 
 		// 4. check update balance
 		if val, err = db.Get(underlayDBBalanceKey, nil); err != nil {
-			continue
 			panic(fmt.Sprintf("get balance failed, error: %s", err))
 		}
 		if balance, ok = big.NewInt(0).SetString(string(val), 10); !ok {
-			continue
 			panic(fmt.Sprintf("covert balance bytes[%s] to big.Int failed", balance))
 		}
 		fmt.Printf("after update balance: %s\n", balance.String())
-		if balance.Int64() != 0 {
-			continue
-			panic("update balance failed")
+		if step == 0{
+			if balance.Int64() != 0 {
+				panic(fmt.Sprintf("amount mismatch, expect: %d, actual: %d", 0, balance.Int64()))
+			}
+		}else if step == 1{
+			if balance.Int64() != 10 {
+				panic(fmt.Sprintf("amount mismatch, expect: %d, actual: %d", 10, balance.Int64()))
+			}
 		}
+
 	}
 
 }
