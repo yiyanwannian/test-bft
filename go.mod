17628c604706a43a78912d51e1971c332de2e3bc
houfazhou <arnordzhou@tencent.com>
2021-11-17 20:27:47 +0800

    use peterh liner prompt

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ludete/test-bft
 
 go 1.15
 
-require github.com/yiyanwannian/client-go v1.0.2
+require (
+	github.com/peterh/liner v1.2.1
+	github.com/yiyanwannian/client-go v1.0.2
+)
--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
-	"bufio"
 	"context"
+	"encoding/hex"
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 	"strings"
 
+	"github.com/peterh/liner"
 	tikvcfg "github.com/yiyanwannian/client-go/config"
 	"github.com/yiyanwannian/client-go/rawkv"
 )
 
 // ./test-bft --endpoint="127.0.0.1:2379"
+
+var (
+	history_fn = filepath.Join(os.TempDir(), ".liner_history")
+)
 
 func main() {
 	var (
@@ -31,35 +38,56 @@ func main() {
 	if len(addrs) == 0 {
 		panic(fmt.Sprintf("endpoint is invalidate: %s", endpoint))
 	}
+
+	line := liner.NewLiner()
+	line.SetCtrlCAborts(true)
+
+	if f, err := os.Open(history_fn); err == nil {
+		_, _ = line.ReadHistory(f)
+		_ = f.Close()
+	}
+
 	db, err := rawkv.NewClient(ctx, addrs, tikvcfg.Default())
 	if err != nil {
 		panic(fmt.Sprintf("Error opening %s by tikvdbprovider: %v", endpoint, err))
 	}
-	defer db.Close()
 
+	defer exitFunc(line, db)
+
+	var keyStr string
 	for {
+		keyStr = ""
 		fmt.Println("------- please input ------")
-		fmt.Print("key: ")
-		reader := bufio.NewReader(os.Stdin)
-		text, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println(fmt.Sprintf("read input err: %v", err))
-			continue
-		}
-		if len(text) < 2 {
-			fmt.Println("empty key")
-			continue
+		if keyStr, err = line.Prompt("key> "); err == nil {
+			line.AppendHistory(keyStr)
+		} else if err == liner.ErrPromptAborted {
+			fmt.Println("exiting")
+			exitFunc(line, db)
+			break
+		} else {
+			fmt.Println(fmt.Sprintf("Error reading line: ", err))
 		}
 
-		text = text[:len(text) -1]
-		val, err1 := db.Get(ctx, []byte(text))
+		val, err1 := db.Get(ctx, []byte(keyStr))
 		if err1 != nil && !strings.Contains(err1.Error(), "not found") {
-			fmt.Println( fmt.Sprintf("can not get key: %s, err: %v", text, err))
+			fmt.Println(fmt.Sprintf("can not get key: %s, err: %v", keyStr, err))
 			continue
 		}
-		fmt.Println(fmt.Sprintf("raw value: %s", string(val)))
-		fmt.Println(fmt.Sprintf("stringed value: %s", string(val)))
+		fmt.Println(fmt.Sprintf("raw value: %s", val))
+		fmt.Println(fmt.Sprintf("hex encoded value: %s", hex.EncodeToString(val)))
 		fmt.Println("-------------------------")
 		fmt.Println()
 	}
+}
+
+func exitFunc(line *liner.State, db *rawkv.Client) {
+	if f, err := os.Create(history_fn); err != nil {
+		log.Print("Error writing history file: ", err)
+	} else {
+		line.WriteHistory(f)
+		f.Close()
+	}
+
+	db.Close()
+	line.Close()
 }
